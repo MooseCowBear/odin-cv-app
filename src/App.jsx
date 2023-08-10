f42985333507aa3d3c98a8851b1c6d90fcf524aa
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-08-10 12:32:54 -0400

    fix typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Header } from './components/Header.jsx'
 import { Form } from './components/Form.jsx';
-import { Cv } from './components/Cv.jsx'
+import { Cv } from './components/CV.jsx'
 
 import './App.css'
 
